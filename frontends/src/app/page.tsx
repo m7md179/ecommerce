49fdda56321b4f255e703c73a1e1a723a5704553
mohammad <mohammad.taha5@hotmail.com>
2024-07-31 09:55:46 +0300

    to commit

--- frontends/src/app/page.tsx
+++ frontends/src/app/page.tsx
@@ -5,21 +5,7 @@ import { IoSearch } from "react-icons/io5";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import  banner from "@/images/banner.jpg"
-import { 
-  Card ,
-  CardTitle,
-  CardDescription,
-  CardContent,
-  CardFooter,
-  CardHeader
-} from "@/components/ui/card"
-import{
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselPrevious,
-  CarouselNext
-} from "@/components/ui/carousel"
+import ScrollItems from "./components/ScrollItems"
 import Navbar from "./components/Navbar";
 import Image from "next/image";
 export default function Home() {
@@ -44,35 +30,7 @@ export default function Home() {
               />
         </div>
         <div className="w-[80vw] h-[100vh] flex justify-center items-center mt-[-100px]">   
-          <Carousel
-            opts={{ align: "start"}}
-            className="w-full"
-          >
-            <CarouselContent>
-            {Array.from({length: 10}).map((_, i) => (
-              <CarouselItem key={i} className="md:basis-1/2 lg:basis-1/4">
-                <Card className="h-[300px] bg-slate-200 shadow-md rounded-2xl p-4 flex items-center justify-between flex-col">
-                  <CardHeader className="space-y-10">
-                    <CardTitle>Header</CardTitle>
-                  </CardHeader>
-                  <CardContent>
-                    <div>
-                      Pic
-                    </div>
-                  </CardContent>
-                  <CardFooter className="flex items-center flex-col space-y-4 p-3">
-                    <CardDescription>
-                      Description
-                    </CardDescription>
-                    <Button>Add to cart</Button>
-                  </CardFooter>
-                </Card>
-              </CarouselItem>
-            ))}
-            </CarouselContent>
-            <CarouselPrevious/>
-            <CarouselNext/>
-          </Carousel>
+          <ScrollItems/>
         </div>
       </section>
     </main>
