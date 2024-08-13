b73462d1159dec68fa43ecc0a0264ab22bf9cde8
mohammad <mohammad.taha5@hotmail.com>
2024-08-13 04:11:42 +0300

    auto changes

--- MyAspNetCoreApp/obj/Debug/net8.0/MyAspNetCoreApp.AssemblyInfo.cs
+++ MyAspNetCoreApp/obj/Debug/net8.0/MyAspNetCoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAspNetCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9b75ff95a9f220e90bb3caf4d5cc8eb60a7ef7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5761f4cb36a023d6adfd3cf997f6fcadc5807c74")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAspNetCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAspNetCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
